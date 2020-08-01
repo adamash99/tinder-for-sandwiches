436e685ca41d919cac4c2d1ffeecc770c0443227
Adam Ash <adamash@berkeley.edu>
2020-08-01 11:42:38 -0700

    added first route

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,28 +3,3 @@ from flask import Flask
 
 app = Flask(__name__)
 from app import routes
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    return app
