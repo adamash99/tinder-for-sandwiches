12490e44bb450117273cffbdffc2de6556f9991e
Adam Ash <adamash@berkeley.edu>
2020-08-26 17:03:24 -0700

    change app running

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__, static_folder='./../../frontend/build', static_url_path='/')
 
-app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+# app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
 
     
 connect(db='tfw', host=constants.MONGO_KEY)
